06ff6fc68a934c6dfe5e3b6213139914d889454d
Jay Park <jay.jongcheol.park@gmail.com>
2018-02-17 23:25:22 -0500

    Add Album Image

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import Card from './Card'
 import CardSection from './CardSection'
 
@@ -9,18 +9,25 @@ const styles = {
     flexDirection: 'column',
     justifyContent: 'space-around',
   },
+  thumbnailStyle: {
+    height: 50,
+    width: 50,
+  },
 }
 
 const AlbumDetail = ({ album }) => {
-  const { headerContentStyle } = styles
+  const { headerContentStyle, thumbnailStyle } = styles
+  const { title, artist, thumbnail_image } = album
 
   return (
     <Card>
       <CardSection>
-        <View />
+        <View>
+          <Image style={thumbnailStyle} source={{ uri: thumbnail_image }} />
+        </View>
         <View style={headerContentStyle}>
-          <Text>{album.title}</Text>
-          <Text>{album.artist}</Text>
+          <Text>{title}</Text>
+          <Text>{artist}</Text>
         </View>
       </CardSection>
     </Card>
@@ -31,6 +38,7 @@ AlbumDetail.propTypes = {
   album: PropTypes.shape({
     title: PropTypes.string.isRequired,
     artist: PropTypes.string.isRequired,
+    thumbnail_image: PropTypes.string.isRequired,
   }).isRequired,
 }
 
