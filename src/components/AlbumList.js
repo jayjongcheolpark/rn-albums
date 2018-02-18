8d5925d34cdd41de41387c171725cbaaf3b966e4
Jay Park <jay.jongcheol.park@gmail.com>
2018-02-17 22:34:04 -0500

    Network Requests

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { View, Text } from 'react-native'
+import axios from 'axios'
 
-const AlbumList = () => (
-  <View>
-    <Text>Album List !!!</Text>
-  </View>
-)
+class AlbumList extends Component {
+  componentDidMount() {
+    axios.get(`https://rallycoding.herokuapp.com/api/music_albums`).then(response => console.log(response))
+  }
+  render() {
+    return (
+      <View>
+        <Text>Album List !!!</Text>
+      </View>
+    )
+  }
+}
 
 export default AlbumList
