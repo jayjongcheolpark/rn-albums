65dfa8818b16333fef2d544125b76c8e59bf4972
Jay Park <jay.jongcheol.park@gmail.com>
2018-02-17 23:33:12 -0500

    Displaying Album Artwork

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -9,26 +9,43 @@ const styles = {
     flexDirection: 'column',
     justifyContent: 'space-around',
   },
+  headerTextStyle: {
+    fontSize: 18,
+  },
   thumbnailStyle: {
     height: 50,
     width: 50,
   },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null,
+  },
 }
 
 const AlbumDetail = ({ album }) => {
-  const { headerContentStyle, thumbnailStyle } = styles
-  const { title, artist, thumbnail_image } = album
+  const { headerContentStyle, thumbnailStyle, thumbnailContainerStyle, headerTextStyle, imageStyle } = styles
+  const { title, artist, thumbnail_image, image } = album
 
   return (
     <Card>
       <CardSection>
-        <View>
+        <View style={thumbnailContainerStyle}>
           <Image style={thumbnailStyle} source={{ uri: thumbnail_image }} />
         </View>
         <View style={headerContentStyle}>
-          <Text>{title}</Text>
+          <Text style={headerTextStyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
+      </CardSection>
+      <CardSection>
+        <Image style={imageStyle} source={{ uri: image }} />
       </CardSection>
     </Card>
   )
@@ -39,6 +56,7 @@ AlbumDetail.propTypes = {
     title: PropTypes.string.isRequired,
     artist: PropTypes.string.isRequired,
     thumbnail_image: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
   }).isRequired,
 }
 
